f9da369d4896eead6ae7a5d50848584b967da759
Daniel Albertazzi Silva <dsilva5211@sdsu.edu>
2021-03-20 13:35:54 -0700

    Added some files for later, fixing header file

--- pagetable/Source.cpp
+++ pagetable/Source.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <stdio.h>
+#include <vector>
 #include <stdlib.h>
 #include <unistd.h>
 #include "pagetable.h"
 
 int main(int argc, char** argv)
 {
-    std::string outPutOp;
+    unsigned int* levels;
+    int levelNum = 0;
+    std::string outPutOp = "";
     int memRefLim;
     int weOutHere = 0;
     int pathing = 0;
@@ -32,7 +35,27 @@ int main(int argc, char** argv)
             std::cout << "wha" << std::endl; 
             break;
         }
-    } 
-    std::cout << argv[optind] << std::endl;
+    }
+    levelNum = argc - optind - 2;
+    levels = new unsigned int[levelNum];
+    for(int i = optind+1, a = 0; i < argc; i++,a++)
+    {
+        levels[a] = atoi(argv[i]);
+        std::cout << levels[a] << std::endl; 
+    }
+    PAGETABLE test = new PAGETABLE(levelNum, levels);
+
+    if (outPutOp != "") 
+    {
+
+    }
+
+    if (memRefLim > 0) 
+    {
+        for (int i = 0; i < memRefLim; i++) 
+        {
+
+        }
+    }
     return 0; 
 }
--- pagetable/pagetable.h
+++ pagetable/pagetable.h
@@ -1,6 +1,13 @@
 #pragma once
 
 struct MAP;
-struct PAGETABLE;
+struct PAGETABLE {
+    int levelCount;
+    unsigned int* numberOfBits;
+    unsigned int* bitMaskAry;
+    unsigned int* shiftAry;
+    int* entryCount;
+    PAGETABLE(int levelCount, unsigned int* numberOfBits);
+};
 struct LEVEL;
 unsigned int LogicalToPage(unsigned int LogicalAddress, unsigned int Mask, unsigned int Shift);
