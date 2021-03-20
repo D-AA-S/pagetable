ac2d021bdad8319f9edbd7562c810af77529df4f
Daniel Albertazzi Silva <dsilva5211@sdsu.edu>
2021-03-19 20:07:26 -0700

    realized other solution was dumb, using simpler solution

--- pagetable/Source.cpp
+++ pagetable/Source.cpp
@@ -6,27 +6,25 @@
 int main(int argc, char** argv)
 {
     int arguments = 0;
+    int weOutHere = 0;
     if (argc < 2) 
     {
         std::cout << "Please enter a file" << std::endl;
         return -1;
     }
 
-    while (()) 
+    while ((weOutHere = getopt(argc, argv, ":n:o:")) != -1) 
     {
-
-    }
-    //some big logical function that uses the getopt function
-
-    if (arguments % 2) 
-    {
-        //-n is active, capture arugment after -n
-        //subtract 1 from arguments to parse for -o argument, if its greater than 1 then we have -o
-    }
-    else if(!(arguments%2))
-    {
-        //execute whatever function argument int represents
-    }
-
-    std::cout << "Hello World!\n";
+        switch (weOutHere)
+        {
+        case 'n':
+            std::cout << "bah" << std::endl;
+            break;
+        case 'o':
+            std::cout << "FAH" << std::endl;
+            break;
+        default:
+            break;
+        }
+    } 
 }
