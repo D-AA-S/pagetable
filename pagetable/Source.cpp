37ac94ddf6c777cc5440d132c8be10769934ea7b
Daniel Albertazzi Silva <dsilva5211@sdsu.edu>
2021-03-19 20:35:50 -0700

    command line argument now properly read

--- pagetable/Source.cpp
+++ pagetable/Source.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include "pagetable.h"
 
 int main(int argc, char** argv)
 {
     int arguments = 0;
-    int weOutHere = 0;
+    int weOutHere;
+
     if (argc < 2) 
     {
         std::cout << "Please enter a file" << std::endl;
         return -1;
     }
-
-    while ((weOutHere = getopt(argc, argv, ":n:o:")) != -1) 
+    while ((weOutHere = getopt(argc, argv, "n:o:")) != -1)
     {
         switch (weOutHere)
         {
@@ -24,8 +25,8 @@ int main(int argc, char** argv)
             std::cout << "FAH" << std::endl;
             break;
         default:
+            std::cout << "wha" << std::endl; 
             break;
         }
-        std::cout << weOutHere << std::endl;
     } 
 }
