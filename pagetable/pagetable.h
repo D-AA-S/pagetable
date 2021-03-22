d1d885dde021aaf545db0983ecd5b39b3282bec2
Kathryn <kshafer7724@sdsu.edu>
2021-03-21 18:21:38 -0700

    Debugged LEVEL and PAGETABLE data structure. Lookin good!

--- pagetable/pagetable.h
+++ pagetable/pagetable.h
@@ -11,7 +11,7 @@ class LEVEL;
 
 class LEVEL
 {
-private: 
+private:
     int DepthOfLevel;   // Which level is this?
     PAGETABLE* PageTablePtr;   // Point to the PageTable that contains the root level node
     std::vector<LEVEL*>* NextLevelPtr;   // Array of pages in this level, each element points to a level node in the next level
@@ -25,27 +25,26 @@ public:
     void PageInsert(PAGETABLE* pageTable, unsigned int LogicalAddress, unsigned int Frame);
 };
 
-class PAGETABLE 
+class PAGETABLE
 {
 private:
-    
+
 
 protected:
     std::vector<unsigned int> numberOfBits; //number of bits per level
     LEVEL* RootNodePtr; // Contains the address of LEVEL 0
-    
-    std::vector<unsigned int> bitMaskArray; //bit masks per level
     std::vector<unsigned int> shiftArray; //bit shift per level
     std::vector<int> entryCount; // EntryCount[i]: # of possible pages for level i 2^8 for example
 
 public:
     int levelCount; //number of levels in the system
     PAGETABLE(int levCount, std::vector<unsigned int> numOfBits);
-    MAP* PageLookup(unsigned int LogicalAddress);
+    //MAP* PageLookup(unsigned int LogicalAddress);
     void PageInsert(unsigned int LogicalAddress, unsigned int Frame);
     unsigned int LogicalToPage(unsigned int LogicalAddress, unsigned int Mask, unsigned int Shift);
     void LevelMaskCalc(std::vector<unsigned int> bitsPerLev);
     void ShiftAryCalc(std::vector<unsigned int> bitsPerLev);
     std::vector<unsigned int> GetBitMask();
+    std::vector<unsigned int> bitMaskArray; //bit masks per level
 };
 
